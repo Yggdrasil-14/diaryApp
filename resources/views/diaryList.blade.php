ec437020d87fd58ada9dc908601297ef0e2c44ff
Yggdrasil-14 <halothinline@gmail.com>
2023-09-04 02:18:42 +0900

    日記なし画面作成

--- resources/views/diaryList.blade.php
+++ resources/views/diaryList.blade.php
@@ -8,11 +8,16 @@
 	crossorigin="anonymous">
 <title>日記一覧</title>
 </head>
+<style>
+body {
+	background-color: moccasin;
+}
+</style>
 <body>
 	<header data-role="header"></header>
 	<br>
 	<div class="container">
-		<div class="col" style= "display:flex">
+		<div class="col" style= "display:flex; margin-bottom: 10%;margin-top: 5%;">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
                 <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
@@ -21,5 +26,15 @@
                 <h2 style= "margin:0 vertical-align: middle;">日記一覧</h2>
 		</div>
 	</div>
+	<img src="{{ asset('img/noDiary.png') }}" alt="" width="500" height="500" style="margin: auto;display: block;">
+	<div style="text-align: center;">
+	<h4>日記が存在しません</h4>
+	<p>＋を押して日記を作成しましょう</p><br>
+	<a href="/" class="example">
+		<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16" style="color: grey;">
+		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+		</svg>
+	</a>
+</div>
 </body>
 </html>
